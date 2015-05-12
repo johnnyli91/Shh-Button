3c366dd9f03e2c1c4dd8409b254858b0232d1a3f
Johnny Li <johnnyli91@gmail.com>
2015-05-11 20:42:29 -0700

    dimmed screen

--- app/src/main/java/co/johnnyli/shh_button/MainActivity.java
+++ app/src/main/java/co/johnnyli/shh_button/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -36,6 +37,9 @@ public class MainActivity extends ActionBarActivity {
                 }
             }
         });
+        WindowManager.LayoutParams WMLP = getWindow().getAttributes();
+        WMLP.screenBrightness = 0.0F;
+        getWindow().setAttributes(WMLP);
     }
 
 
