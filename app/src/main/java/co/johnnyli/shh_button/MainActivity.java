c9e5150c682d63308f86cf5b2539563e5ea01ad3
Johnny Li <johnnyli91@gmail.com>
2015-05-21 20:00:30 -0700

    changed to four button layout

--- app/src/main/java/co/johnnyli/shh_button/MainActivity.java
+++ app/src/main/java/co/johnnyli/shh_button/MainActivity.java
@@ -1,94 +1,61 @@
 package co.johnnyli.shh_button;
 
+import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.CheckBox;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    public static int count;
-    public static boolean dim;
-    private CheckBox autoDim;
-    public static int button;
+public class MainActivity extends Activity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ImageView ShhButton = (ImageView) findViewById(R.id.button_image);
-        ShhButton.setImageResource(R.drawable.normal_button);
-        final TextView Counter = (TextView) findViewById(R.id.counter);
-        Counter.setText("Count: " + count);
+        ImageView Button1 = (ImageView) findViewById(R.id.button1);
+        Button1.setImageResource(R.drawable.yellow_face);
+        ImageView Button2 = (ImageView) findViewById(R.id.button2);
+        Button2.setImageResource(R.drawable.green_face);
+        ImageView Button3 = (ImageView) findViewById(R.id.button3);
+        Button3.setImageResource(R.drawable.red_face);
+        ImageView Settings = (ImageView) findViewById(R.id.settings);
+        Settings.setImageResource(R.drawable.the_settings);
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.test);
-        ShhButton.setOnClickListener(new View.OnClickListener() {
+        Button1.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 if (!mp.isPlaying()) {
                     mp.start();
-                    count++;
-                    Counter.setText("Count: " + count);
-                    if (button == 0) {
-                        ShhButton.setImageResource(R.drawable.awkward_button);
-                        button++;
-                    } else if (button == 1) {
-                        ShhButton.setImageResource(R.drawable.angry_button);
-                        button++;
-                    } else {
-                        ShhButton.setImageResource(R.drawable.normal_button);
-                        button = 0;
-                    }
-                }
-            }
-        });
-        final WindowManager.LayoutParams WMLP = getWindow().getAttributes();
-        autoDim = (CheckBox) findViewById(R.id.autoDim);
-        autoDim.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dim = autoDim.isChecked();
-                if (dim) {
-                    WMLP.screenBrightness = 0.0F;
-                    getWindow().setAttributes(WMLP);
-                } else {
-                    WMLP.screenBrightness = 0.3F;
-                    getWindow().setAttributes(WMLP);
                 }
             }
         });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-//            Intent settings = new Intent(this, SettingsActivity.class);
-//            startActivity(settings);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+////            Intent settings = new Intent(this, SettingsActivity.class);
+////            startActivity(settings);
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
