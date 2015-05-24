e935528ba939d5e295383c72d94abf08fb69ed6f
Johnny Li <johnnyli91@gmail.com>
2015-05-23 22:47:56 -0700

    added couple things to settings

--- app/src/main/java/co/johnnyli/shh_button/Settings.java
+++ app/src/main/java/co/johnnyli/shh_button/Settings.java
@@ -2,6 +2,8 @@ package co.johnnyli.shh_button;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class Settings extends ActionBarActivity {
@@ -10,27 +12,19 @@ public class Settings extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        TextView text = (TextView) findViewById(R.id.settings_text);
+        text.setText("This is the settings page.");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_settings, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
